dd2077ef32fdc5f44710be6d26d30de684f1efc3
killingcammand <154828355+killingcammand@users.noreply.github.com>
2025-07-04 14:21:43 +0530

    second changes

--- frontend/src/components/OrderSummary.jsx
+++ frontend/src/components/OrderSummary.jsx
@@ -6,7 +6,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import axios from "../lib/axios";
 
 const stripePromise = loadStripe(
-	"pk_test_51KZYccCoOZF2UhtOwdXQl3vcizup20zqKqT9hVUIsVzsdBrhqbUI2fE0ZdEVLdZfeHjeyFXtqaNsyCJCmZWnjNZa00PzMAjlcL"
+	STRIPE_SECRET_KEY
 );
 
 const OrderSummary = () => {
